d47d024671b1be15e8f3587482356eb1de2a6569
hieupmse05244 <hieupmse05244@fpt.edu.vn>
2019-08-19 15:45:58 +0700

    refactor code

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
-if [ $JENKINS_SERVER ]; then
+if [ $JENKINS_SERVER ] 
+then
     sed -i "s~http://jenkins_server:8080~$JENKINS_SERVER~g" config.ini
 fi
 
-if [ $JENKINS_USERNAME ]; then
+if [ $JENKINS_USERNAME ]
+then
     sed -i "s/username/$JENKINS_USERNAME/g" config.ini
 fi
 
-if [ $JENKINS_PASSWORD ]; then
+if [ $JENKINS_PASSWORD ]
+then
     sed -i "s/password/$JENKINS_PASSWORD/g" config.ini
 fi
 
-# Run exporter
 /usr/local/bin/python main.py
